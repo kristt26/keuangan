d2c73469296d12487ef29977a07dc6a76a87dbd5
kristt26 <kristt26@gmail.com>
2020-06-02 17:28:38 +0900

    ok

--- api/datas/create/CreatePembayaranByMhs.php
+++ api/datas/create/CreatePembayaranByMhs.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
